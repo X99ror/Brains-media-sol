0da6fd375590d0c49f4379378e1bcb74176c03e3
X99ror <xgeror0062@gmail.com>
2024-07-23 10:39:52 +0530

    resolved an issue by brute force

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,26 +1,17 @@
-import "./styles/Hero.css"
-import React from 'react'
-import IntroImg from "../assets/intro.jpg"
-
+import React from 'react';
+import './styles/Hero.css';
 
 const Hero = () => {
   return (
     <div className="hero">
-        <div className="mask">
-            <img className="intro-img" src={IntroImg} alt="Space" />
-
-        </div>
-        <div className="content">
-          
-        <h1>"At Brains Media Solutions, we transform your ideas into dynamic digital experiences."</h1>
-        
-        
-       
-          
-        
-        </div>
+      <div className="mask">
+        <img className="intro-img" src={`${process.env.PUBLIC_URL}/assets/intro4.jpg`} alt="Space" />
+      </div>
+      <div className="content">
+        <h1>At Brains Media Solutions, we transform your ideas into dynamic digital experiences.</h1>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
